85901e8fecbbdd507de33c5ac7f554f4a56de13e
Chris Padilla <cdpadilla42@gmail.com>
2020-08-20 11:05:35 -0500

    Correct text flipping at line ends

--- mock-up/async-typer.js
+++ mock-up/async-typer.js
@@ -6,19 +6,37 @@ function getRandomBetween(min = 10, max = 15, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 
-// async for of loop
 async function draw(el) {
   const text = el.textContent;
-  let soFar = '';
+  const html = `
+    <span class="show"></span><span class="hide" style="color: rgba(0,0,0,0);"></span>`;
+  el.innerHTML = html;
+  const showField = el.querySelector('.show');
+  const hideField = el.querySelector('.hide');
+  console.log(showField);
+  let hideText = text;
+  let showText = '';
   for (const letter of text) {
-    soFar += letter;
-    el.textContent = soFar;
-    // wait for some amount of time
+    showText += letter;
+    hideText = hideText.slice(1);
+    showField.textContent = showText;
+    hideField.textContent = hideText;
     const { typeMin, typeMax } = el.dataset;
-    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+    const amountOfTimeToWait = getRandomBetween(typeMin, 100);
     await wait(amountOfTimeToWait);
   }
 }
+
+// async for of loop
+// async function draw(el) {
+//   const text = el.textContent;
+//   let soFar = '';
+//   for (const letter of text) {
+//     soFar += letter;
+//     el.textContent = soFar;
+//     // wait for some amount of time
+//   }
+// }
 
 const text = document.querySelector('.text_box__text');
 const buttons = document.querySelectorAll('button');
