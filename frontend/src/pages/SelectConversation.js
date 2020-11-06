91754918db8014b73428aca8fe5871460d751137
Chris Padilla <cdpadilla42@gmail.com>
2020-11-05 20:52:14 -0600

    Render conversation names to the conversation selection page

--- frontend/src/pages/SelectConversation.js
+++ frontend/src/pages/SelectConversation.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getConversations } from '../store/conversations';
 
 const SelectConversation = () => {
   const dispatch = useDispatch();
+  let conversations = useSelector((state) => state.conversations.conversations);
+  // Above does not return a true array, below converts data to an array with the map method available to it
+  conversations = [...conversations];
 
   useEffect(() => {
     dispatch(getConversations());
   }, []);
 
+  function renderConversations() {
+    console.log(conversations);
+
+    return conversations.map((convo) => (
+      <li key={convo._id} id={convo._id}>
+        {convo.name}
+      </li>
+    ));
+  }
+
   return (
     <div>
-      <ul>
-        <li>Hello!</li>
-      </ul>
+      <ul>{renderConversations()}</ul>
     </div>
   );
 };
