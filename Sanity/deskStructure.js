b56719754512930c012461df4e29382474f644c7
Chris Padilla <cdpadilla42@gmail.com>
2020-11-07 16:06:06 -0600

    Add Sanity files and Animal Document

---
+++ Sanity/deskStructure.js
@@ -0,0 +1,29 @@
+import S from '@sanity/desk-tool/structure-builder';
+
+export default () =>
+  S.list()
+    .title('Content')
+    .items([
+      // Make a new list item
+      S.listItem()
+        .title('Dialogue by Conversation')
+        .child(
+          // List out the categories
+          S.documentTypeList('conversation')
+            .title('Dialogue by Conversation')
+            // When a category is selected, pass its id down to the next pane
+            .child((conversationID) =>
+              // load a new document list
+              S.documentList()
+                .title('Projects')
+                // Use a GROQ filter to get documents.
+                // This filter checks for sampleProjects that has the
+                // categoryId in its array of references
+                .filter(
+                  '_type == "dialogue" && $conversationID == conversation._ref'
+                )
+                .params({ conversationID })
+            )
+        ),
+      ...S.documentTypeListItems(),
+    ]);
