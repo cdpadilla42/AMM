22915ef316a3d37243c5bfd16db32b356c25834d
Chris Padilla <cdpadilla42@gmail.com>
2020-11-07 15:46:25 -0600

    Break one unnecessary level of nesting in Sanity file structure

--- mock-up/deskStructure.js
+++ mock-up/deskStructure.js
@@ -6,34 +6,24 @@ export default () =>
     .items([
       // Make a new list item
       S.listItem()
-        // Give it a title
-        .title('Folders')
+        .title('Dialogue by Conversation')
         .child(
-          S.list()
-            .title('Folders')
-            .items([
-              // Add a new parent list item
-              S.listItem()
-                .title('Dialogue by Conversation')
-                .child(
-                  // List out the categories
-                  S.documentTypeList('conversation')
-                    .title('Dialogue by Conversation')
-                    // When a category is selected, pass its id down to the next pane
-                    .child((conversationID) =>
-                      // load a new document list
-                      S.documentList()
-                        .title('Projects')
-                        // Use a GROQ filter to get documents.
-                        // This filter checks for sampleProjects that has the
-                        // categoryId in its array of references
-                        .filter(
-                          '_type == "dialogue" && $conversationID == conversation._ref'
-                        )
-                        .params({ conversationID })
-                    )
-                ),
-            ])
+          // List out the categories
+          S.documentTypeList('conversation')
+            .title('Dialogue by Conversation')
+            // When a category is selected, pass its id down to the next pane
+            .child((conversationID) =>
+              // load a new document list
+              S.documentList()
+                .title('Projects')
+                // Use a GROQ filter to get documents.
+                // This filter checks for sampleProjects that has the
+                // categoryId in its array of references
+                .filter(
+                  '_type == "dialogue" && $conversationID == conversation._ref'
+                )
+                .params({ conversationID })
+            )
         ),
       ...S.documentTypeListItems(),
     ]);
