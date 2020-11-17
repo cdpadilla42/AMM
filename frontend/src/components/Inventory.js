ae8c322e7163341c330945a06817ff7ffd83b697
Chris Padilla <cdpadilla42@gmail.com>
2020-11-17 16:25:56 -0600

    Create inventory visual prototype

--- frontend/src/components/Inventory.js
+++ frontend/src/components/Inventory.js
@@ -1,16 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledInventory = styled.div`
+  width: 540px;
+  height: 50%;
+  border: 1px solid black;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1rem;
+  padding: 1rem;
+  background-color: palegoldenrod;
+  border-radius: 5px;
+
+  div {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.4rem 0;
+    background-color: #fff;
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+  }
+
+  span {
+    display: block;
+    flex: 1;
+  }
+`;
 
 const Inventory = () => {
-  const inventory = useSelector((store) => store.inventory);
-  console.log(inventory);
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  // const inventory = useSelector((store) => store.inventory);
+  // console.log(inventory);
+  const dummyInventory = new Array(6);
+  dummyInventory.fill({
+    name: 'horse',
+    image: 'https://f4.bcbits.com/img/a3261223391_2.jpg',
+  });
 
   function renderInventory() {
-    return inventory.map((item) => {
-      return <p key={item.name}>{item.name}</p>;
+    return dummyInventory.map((item) => {
+      return (
+        <div key={item.name}>
+          <img src={item.image} alt="" />
+          <span>{item.name}</span>
+        </div>
+      );
     });
   }
-  return <div>{renderInventory()}</div>;
+  return (
+    <>
+      {isDetailsOpen ? (
+        <ItemDetailsDisplay />
+      ) : (
+        <StyledInventory>{renderInventory()}</StyledInventory>
+      )}{' '}
+      <img
+        src="https://www.cheatcc.com/imageswii/phoenixwrightaceattorney_12a.jpg"
+        alt=""
+      />{' '}
+    </>
+  );
+};
+
+const ItemDetailsDisplay = () => {
+  return <div>It's a horse!</div>;
 };
 
 export default Inventory;
