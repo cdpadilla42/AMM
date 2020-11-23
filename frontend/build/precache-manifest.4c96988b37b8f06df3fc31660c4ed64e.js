f0a94323b7ea688d6c2b1563a73d3b82f1e27597
Chris Padilla <cdpadilla42@gmail.com>
2020-11-22 18:34:24 -0600

    Build app

---
+++ frontend/build/precache-manifest.4c96988b37b8f06df3fc31660c4ed64e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d514ae9906079af0ee1e5ea1bdf8c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "973abdfbda7afef2c1d4",
+    "url": "/static/css/main.380b8427.chunk.css"
+  },
+  {
+    "revision": "b2ba1ca9cd136aecb053",
+    "url": "/static/js/2.761c06ff.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/static/js/2.761c06ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973abdfbda7afef2c1d4",
+    "url": "/static/js/main.81161865.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "439432919f79fbb322f96874de6e6a18",
+    "url": "/static/media/laugh.43943291.png"
+  },
+  {
+    "revision": "57935f5407a1467328469b9c707e3d48",
+    "url": "/static/media/mad.57935f54.png"
+  },
+  {
+    "revision": "0eabf7562f392bcd9d20aef033f5419b",
+    "url": "/static/media/normal.0eabf756.png"
+  },
+  {
+    "revision": "e0af5c1101e56117d0cc7f9c1aab7992",
+    "url": "/static/media/sad.e0af5c11.png"
+  },
+  {
+    "revision": "9e152b90e062452cd3d4121e0a3cb6a5",
+    "url": "/static/media/sleep.9e152b90.png"
+  }
+]);
