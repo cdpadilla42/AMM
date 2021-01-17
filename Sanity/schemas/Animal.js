828f8d6bd7c75863664c0217a89ad1b3bff992c2
Chris Padilla <cdpadilla42@gmail.com>
2021-01-17 14:12:17 -0600

    Add color picker to Sanity

--- Sanity/schemas/Animal.js
+++ Sanity/schemas/Animal.js
@@ -9,9 +9,16 @@ export default {
       type: 'string',
     },
     {
-      title: 'Neutral Image',
+      title: 'Icon Image',
       name: 'normal',
+      description: 'Not used in game',
       type: 'image',
+    },
+    {
+      title: 'Color',
+      name: 'color',
+      description: `Used in name tag and collored accents. Must be hex color. Ex: "#BADA55"`,
+      type: 'color',
     },
   ],
 };
