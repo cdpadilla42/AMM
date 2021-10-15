6cf523ccc30346a8c314898231aeb2561f81eb66
Chris Padilla <cdpadilla42@gmail.com>
2021-10-15 13:13:05 -0500

    Potential fix for iPhone viewport issue - JS and custom properties

--- frontend/src/pages/Testimony.js
+++ frontend/src/pages/Testimony.js
@@ -53,6 +53,25 @@ const Testimony = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    const handleWindowResize = () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    }
+  }, []);
+
   // Get items for inventory, Animal notes & Agent S Notes
   useEffect(() => {
     dispatch(getInventoryItems());
