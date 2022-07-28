6886389fdc5c75942616d444b58290d27caf4371
Chris Padilla <cdpadilla42@gmail.com>
2022-07-28 15:09:23 -0500

    Add Nenns letter text

--- frontend/src/pages/Letter.js
+++ frontend/src/pages/Letter.js
@@ -5,10 +5,38 @@ const Letter = () => {
   return (
     <StyledLetter>
       <section className="letter_container">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore dolor
-        assumenda maxime praesentium sint neque voluptatum quas ab quo accusamus
-        quia, quis, rerum repudiandae dolores hic numquam placeat itaque nihil.
-        <figure>- Ñen</figure>
+        <h2>**LOOKING FOR A DETECTIVE**</h2>
+
+        <p>
+          A murder attempt happened last night with the local pupper, Lucky, as
+          the victim! I was the one who discovered the crime scene so the
+          villagers are blaming me! I need someone to clear my name and find the
+          true culprit!
+        </p>
+
+        <p>
+          I’m looking for a detective who can find the truth by talking to a
+          whole cast of unique characters, all with their own point of view.
+          This detective would also need to be able to search my in-game island
+          for evidence to use in an Ace-Attorney-like court system to make their
+          case. If this sounds interesting to you, please come. :,c
+        </p>
+
+        <p>
+          To get here, you’ll need a Nintendo Switch, a copy of Animal Crossing:
+          New Horizons, and a Nintendo Online Subscription. If you lay on a bed
+          in your in-game home, you’ll be able to travel to my dream island
+          through my address. I’ll be waiting near my house in the center of the
+          island with more information.
+        </p>
+
+        <p>
+          If you have the time and skills please reach me via my dream code: DA
+          5197-5510-7359
+        </p>
+
+        <p>Please help :C,</p>
+        <figure>- Ñenn</figure>
       </section>
     </StyledLetter>
   );
